a7c1081d528e877ff40a05632a79788cdfaca533
jon-wade <jonwadeuk@gmail.com>
2016-11-11 19:01:08 +0000

    minor refactoring to add new routes

--- src/AppBundle/Controller/ToDoController.php
+++ src/AppBundle/Controller/ToDoController.php
@@ -10,7 +10,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
+class ToDoController extends Controller
 {
     /**
      * @Route("/", name="homepage")
@@ -45,4 +45,12 @@ class DefaultController extends Controller
             'form' => $form->createView()
         ));
     }
+
+    /**
+     * Matches /delete/*
+     *
+     * @Route("/delete/{id}", name="delete", requirements={"id": "\d+"})
+     */
+
+
 }
