424017b284dffcb716b6748f9bb199f83d4d460c
jon-wade <jonwadeuk@gmail.com>
2016-11-11 19:47:03 +0000

    add in an update route

--- src/AppBundle/Controller/ToDoController.php
+++ src/AppBundle/Controller/ToDoController.php
@@ -12,9 +12,13 @@ use Symfony\Component\HttpFoundation\Request;
 
 class ToDoController extends Controller
 {
+
     /**
+     * @param Request $request
      * @Route("/", name="homepage")
+     * @return Response
      */
+
     public function indexAction(Request $request)
     {
         $taskInput = new TaskInput();
@@ -48,14 +52,15 @@ class ToDoController extends Controller
 
     /**
      * Matches /delete/*
-     *
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
      * @Route("/delete/{id}", name="delete", requirements={"id": "\d+"})
      */
+
     public function deleteAction($id) {
 
         $em = $this->getDoctrine()->getManager();
         $repository = $this->getDoctrine()->getRepository('AppBundle:Task');
-
         $task = $repository->find($id);
 
         if (!$task) {
@@ -70,5 +75,32 @@ class ToDoController extends Controller
         return $this->redirectToRoute('homepage');
     }
 
+    /**
+     * Matches /update/*
+     * @param $id
+     * @param $value
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     * @Route("/update/{id}/{value}", name="update", requirements={"id": "\d+"})
+     */
+
+    public function updateAction($id, $value) {
+
+        $em = $this->getDoctrine()->getManager();
+        $repository = $this->getDoctrine()->getRepository('AppBundle:Task');
+        $task = $repository->find($id);
+
+        if (!$task) {
+            throw $this->createNotFoundException(
+                'No task found for id '.$id
+            );
+        }
+
+        //TODO need to pass in the parameter to update the task
+        $task->setItem($value);
+        $em->flush();
+
+        return $this->redirectToRoute('homepage');
+
+    }
 
 }
